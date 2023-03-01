4ed8ebdb26ce3ce2c58c3ce6eda68e1056bb8bf9
xyr10 <124945147+xyr10@users.noreply.github.com>
2023-02-28 18:30:01 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("Script");
+
+let youtSearch = "Search";
+let youtTitle = "Title";
+let youtSubscribe = "Not Subscribed";
+let youtNotification = "Bell selected";
+let youtShare = "Open Link";
+let youtLikes = "Thumbs Up";
+let youtDescription = "This is a video";
+let youtViews = "4,000";
+let youtComments = "Enabled";
+let youtMerchandise = "Jacket";
+let thumbnails = "img/MusicVideo.mov";
+
+document.write("<h1>" + youtTitle + "</h1>";
+document.write("<p>" + youtDescription + "</p>");
+
+document.write("<img src='" + thumbnails+ "' alt='Something'> ");
